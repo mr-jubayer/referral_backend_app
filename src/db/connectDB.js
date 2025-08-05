9765945a30ae05106e001540c86928cac13ab558
jubayer <jubayerahmed.dev@gmail.com>
2025-08-05 22:41:01 +0600

    mongodb uri setup

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
-const uri = `mongodb://localhost:27017/test`;
+// const uri = `mongodb://localhost:27017/test`;
 // `${process.env.MONGODB_URI}/${DB_NAME}`
+const uri = process.env.MONGODB_URI;
 
 const connection = {};
 const connectDB = async () => {
