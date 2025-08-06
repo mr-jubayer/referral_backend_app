2e1aeaa90ab7cce77f9871171036d3a91e31f921
jubayer <jubayerahmed.dev@gmail.com>
2025-08-06 14:01:23 +0600

    removed all logs

--- src/db/connectDB.js
+++ src/db/connectDB.js
@@ -7,18 +7,13 @@ const uri = `mongodb://localhost:27017/test`;
 const connection = {};
 const connectDB = async () => {
   if (connection.isConnected) {
-    console.log("Database is already connected");
     return;
   }
 
   try {
     const connectionInstance = await mongoose.connect(uri);
     connection.isConnected = connectionInstance.connections[0].readyState;
-    console.log(
-      `MongoDB Connected! DB Host: ${connectionInstance.connection.host}`
-    );
   } catch (error) {
-    console.error("MongoDB connection FAILED !!! ", error);
     process.exit(1);
   }
 };
